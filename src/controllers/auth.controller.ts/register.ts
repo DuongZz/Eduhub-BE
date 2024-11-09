38b4bebbe39deec4c0b277b4d23b1d9bc1e84409
sontransonn <sontransonn@gmail.com>
2024-11-09 20:58:23 +0700

    change file register

--- src/controllers/auth.controller.ts/register.ts
+++ src/controllers/auth.controller.ts/register.ts
@@ -2,40 +2,32 @@ import { StatusCodes } from 'http-status-codes';
 import { Response, Request } from "express";
 import User from "../../models/user";
 import bcrypt from 'bcrypt';
-"jelo"
 
 export const register = async (req: Request, res: Response) => {
   try {
-    const { email, password, fullName, gender, dateOfBirth, country, province, province_city, phone, avatar } = req.body;
+    const { fullName, email, phone, password } = req.body;
     const user = await User.findOne({ email });
+
     if (!password) {
       return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Password is required.' });
     }
+
     if (user) {
       return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Email existing.' });
     }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = new User({
-      email,
-      password: hashedPassword,
       fullName,
-      gender,
-      dateOfBirth,
-      country,
-      province,
-      province_city,
+      email,
       phone,
-      avatar,
+      password: hashedPassword,
     })
     await newUser.save();
-    res.status(StatusCodes.OK).json({
-      message: 'Registered successfully',
-      user: {
-        id: newUser._id, email: newUser.email,
-        fullName: newUser.fullName
-      }
-    });
+
+    res.status(StatusCodes.OK).json({ message: 'Registered successfully' });
   } catch (err) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
   }
