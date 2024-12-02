ac99299fa886af022a9fa2e55e2b0e98db169c29
sontransonn <sontransonn@gmail.com>
2024-12-02 17:02:08 +0700

    chance slug

--- src/services/course/getCourseByOptionsService.ts
+++ src/services/course/getCourseByOptionsService.ts
@@ -7,13 +7,13 @@ export const getCourseByOptionService = async (option: string, page: number) => 
 
     let courses: any;
 
-    if (option === 'top-sold') {
+    if (option === 'top-ban-chay') {
       courses = await Course.find().sort({ sold: -1 }).skip(skip).limit(limit);
-    } else if (option === 'top-sale') {
+    } else if (option === 'top-uu-dai') {
       courses = await Course.find().sort({ discount: -1 }).skip(skip).limit(limit);
-    } else if (option === 'top-viewed') {
+    } else if (option === 'khoa-hoc-duoc-xem-nhieu') {
       courses = await Course.find().sort({ view: -1 }).skip(skip).limit(limit);
-    } else if (option === 'new-released') {
+    } else if (option === 'khoa-hoc-moi-ra-mat') {
       courses = await Course.find().sort({ createdAt: -1 }).skip(skip).limit(limit);
     }
 
