053fd105e48d9b5f2141aa8333fa4fe947625ab5
duongdaoq <duongdaoq@gmail.com>
2024-12-13 23:11:11 +0700

    .

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { checkJwt } from "../../middlewares/authMiddleware";
-import { checkRoleAdmin } from "../../middlewares/checkRole";
-import { getAllApplicationController } from "../../controllers/admin.controller/getAllApplicationController";
-import { getAApplicationController } from "../../controllers/admin.controller/getAApplicationController";
-import { changeRoleController } from "../../controllers/admin.controller/changeRoleController";
+import { checkJwt } from "../middlewares/authMiddleware";
+import { checkRoleAdmin } from "../middlewares/checkRole";
+import { getAllApplicationController } from "../controllers/admin.controller/getAllApplicationController";
+import { getAApplicationController } from "../controllers/admin.controller/getAApplicationController";
+import { changeRoleController } from "../controllers/admin.controller/changeRoleController";
 
 const router = Router();
 router.use(checkJwt);
