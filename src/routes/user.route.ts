c741b1e08abd0545004cafedd7f5eeed9c062403
duongdaoq <duongdaoq@gmail.com>
2024-12-13 22:49:27 +0700

    merge to dev

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,19 @@
 
 import { Router } from "express";
-
-import { getUserInfo } from "../controllers/user.controller";
-
 import { checkJwt } from "../middlewares/authMiddleware";
+import { applyInstructorController } from "../controllers/user.controller/applyInstructorController";
+import { upload } from "../middlewares/uploadCVMulter";
+import { addCourseToCartController } from "../controllers/user.controller/addCourseToCartController";
+import { getMyCartController } from "../controllers/user.controller/getMyCartController";
+import { getUserInfo } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get("/", checkJwt, getUserInfo)
+router.use(checkJwt)
+router.post('/apply', upload.single('cv'), applyInstructorController);
+router.post('/add-to-cart', addCourseToCartController);
 
-// router.post('/change-password', changePassword)
+router.get('/my-cart', getMyCartController);
+router.get("/", getUserInfo)
+
 export default router;
