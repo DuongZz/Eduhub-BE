db6ad63b0f79824274109a187ec545f15212804d
duongdaoq <duongdaoq@gmail.com>
2024-12-21 22:16:09 +0700

    fix 25 instructor

--- src/services/instructor/getAllInstructorService.ts
+++ src/services/instructor/getAllInstructorService.ts
@@ -1,7 +1,7 @@
 import Instructor from "../../models/instructor";
 export const getAllInstructorService = async (page: number) => {
   try {
-    const limit = 2;
+    const limit = 25;
     const skip = (page - 1) * limit;
     const instructor = await Instructor.find().skip(skip).limit(limit).populate("user");
     return instructor;
