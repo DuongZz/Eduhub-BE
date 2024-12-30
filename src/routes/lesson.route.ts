d2d0c2b3407d148738bf8dd36e0f312a933b8dc7
duongdaoq <duongdaoq@gmail.com>
2024-12-30 20:15:20 +0700

    uplinkYtb module

--- src/routes/lesson.route.ts
+++ src/routes/lesson.route.ts
@@ -4,8 +4,9 @@ import { checkRoleInstructor } from "../middlewares/checkRole";
 import { checkJwt } from "../middlewares/authMiddleware";
 import { getALessonBySlugController } from "../controllers/lesson.controller/getALessonBySlugController";
 import { videoUploadMiddleware } from "../middlewares/uploadVideoMiddleware";
+import { uploadLinkYoutubeController } from "../controllers/lesson.controller/uploadLinkYoutubeController";
 const router = Router();
 router.get('/:courseSlug/:lessonId', getALessonBySlugController)
 router.post('/upload/:courseId', checkJwt, checkRoleInstructor, videoUploadMiddleware, uploadLessonController)
-
+router.post('/uplink/:courseId', checkJwt, checkRoleInstructor, uploadLinkYoutubeController)
 export default router
