360fd07f3b848555e4648118e06fd9f29f73b01e
sontransonn <sontransonn@gmail.com>
2025-01-10 16:02:54 +0700

    fix

--- src/services/course/getCourseByCategoryService.ts
+++ src/services/course/getCourseByCategoryService.ts
@@ -6,13 +6,12 @@ export const getCourseByCategoryService = async (category: string, page: number)
     const skip = (page - 1) * limit;
 
     const courses = await Course.find({ category }).skip(skip).limit(limit);
-
+    const total = await Course.countDocuments({ category });
+    const totalPages = Math.ceil(total / limit);
     const suggestedCourses = await Course.find({ _id: { $nin: courses.map(c => c._id) } })
       .sort({ view: -1 })
       .limit(8);
-    console.log(suggestedCourses)
-
-    return { courses, suggestedCourses };
+    return { courses, suggestedCourses, total, totalPages };
   } catch (err) {
     throw new Error(err.message);
   }
