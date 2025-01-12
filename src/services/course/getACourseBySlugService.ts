61a6d148bbe7f9d3549e0d19c9edaaabd724781f
duongdaoq <duongdaoq@gmail.com>
2025-01-12 13:03:20 +0700

    fix getACourseBySlug

--- src/services/course/getACourseBySlugService.ts
+++ src/services/course/getACourseBySlugService.ts
@@ -1,10 +1,18 @@
 import Course from '../../models/course';
+import Instructor from '../../models/instructor';
 
 export const getACourseBySlugService = async (slug: string) => {
   try {
     const course = await Course.findOne({ slug: slug })
-      .populate("approvedBy", "fullName")
+      .populate("approvedBy")
       .populate("videos", "lessonName");;
+
+    if (course && course.approvedBy) {
+      const instructor = await Instructor.findOne({ user: course.approvedBy._id })
+        .populate('user')
+        .exec();
+      course.approvedBy = instructor;
+    }
     return course;
   } catch (err) {
     throw new Error(err)
