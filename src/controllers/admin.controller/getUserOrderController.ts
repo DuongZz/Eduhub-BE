fffe5dabbad3f7b4ab19983eb6de7611ca1d1380
duongdaoq <duongdaoq@gmail.com>
2025-01-16 12:36:37 +0700

    fix getUserOrder get by status

--- src/controllers/admin.controller/getUserOrderController.ts
+++ src/controllers/admin.controller/getUserOrderController.ts
@@ -4,10 +4,10 @@ import { getUserOrderService } from '../../services/admin/getUserOrderService';
 
 export const getUserOrderController = async (req: Request, res: Response) => {
   try {
-    const { role, page } = req.query;
+    const { orderStatus, page } = req.query;
     const pageNum = page ? Number(page) : 1;
 
-    const userOrder = await getUserOrderService(role as string, pageNum);
+    const userOrder = await getUserOrderService(orderStatus as string, pageNum);
     res.status(StatusCodes.OK).json(userOrder);
   } catch (err) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
--- src/services/admin/getUserOrderService.ts
+++ src/services/admin/getUserOrderService.ts
@@ -1,20 +1,14 @@
 import Order from "../../models/order";
 import User from "../../models/user";
 
-export const getUserOrderService = async (role: string, page: number) => {
+export const getUserOrderService = async (orderStatus: string, page: number) => {
   try {
     const limit = 8;
     const skip = (page - 1) * limit;
 
-    const usersWithRole = await User.find({ role }).select('_id');
-    if (usersWithRole.length === 0) {
-      return { message: 'Không tìm thấy người dùng với role này' };
-    }
-
-    const userOrder = await Order.find({ user: { $in: usersWithRole.map(user => user._id) } })
+    const userOrder = await Order.find({ paymentStatus: orderStatus })
       .populate({
         path: 'user',
-        match: { role },
         select: 'fullName role',
       })
       .skip(skip)
