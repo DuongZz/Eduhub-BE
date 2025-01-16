4a17d68be44399b3cbecf73f95a03a604a34b7d9
duongdaoq <duongdaoq@gmail.com>
2025-01-16 15:20:05 +0700

    fix get các comment mới nhất

--- src/services/admin/getAllCommentService.ts
+++ src/services/admin/getAllCommentService.ts
@@ -4,7 +4,10 @@ export const getAllCommentAdminService = async (page: number) => {
   try {
     const limit = 8;
     const skip = (page - 1) * limit;
-    const comment = await Comment.find().skip(skip).limit(limit);
+    const comment = await Comment.find()
+      .sort({ commentedDate: -1 })
+      .skip(skip)
+      .limit(limit);
     if (!comment) {
       return { message: 'Không có comment nào' };
     }
