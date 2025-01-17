511d140f2aab44bdc224f3b9086a2cde599417a2
duongdaoq <duongdaoq@gmail.com>
2025-01-17 19:25:16 +0700

    fix cors

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import cors from "cors"
 
 const corsOption = {
-    origin: ['https://eduhub-zvij.onrender.com/', 'http://localhost:3000'],
+    origin: ['https://eduhub-zvij.onrender.com/', 'https://eduhub-zvij.onrender.com/'],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     optionsSuccessStatus: 204,
     credentials: true
