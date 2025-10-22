c6bc6ddd1e120fe0721d7f1ba78f716622a73053
duongdaoq <duongdaoq@gmail.com>
2025-10-22 23:13:17 +0700

    refactor corsconfig

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import cors from "cors"
 
 const corsOption = {
-    origin: ['https://eduhub-fe-eight.vercel.app/', 'https://eduhub-fe-eight.vercel.app/'],
+    origin: ['https://eduhub-fe-eight.vercel.app', 'https://eduhub-fe-eight.vercel.app'],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     optionsSuccessStatus: 204,
     credentials: true
